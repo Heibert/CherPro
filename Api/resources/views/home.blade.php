90fc5c772ca6fa60b6da2187b0a6beace2c7d0a5
Usuario <Email Usuario>
2022-09-13 11:47:32 -0500

    css

--- Api/resources/views/home.blade.php
+++ Api/resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link  href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    @vite(['resources/css/app.css'])
     <title>Document</title>
 </head>
 <body>
