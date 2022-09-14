8a8161c69e47e97e77d748f7c5f2f82fbb5900d6
Usuario <Email Usuario>
2022-09-14 07:50:20 -0500

    Seeders #3

--- Api/database/factories/CoordinacionFactory.php
+++ Api/database/factories/CoordinacionFactory.php
@@ -19,7 +19,7 @@ class CoordinacionFactory extends Factory
     public function definition()
     {
         return [
-            "nomCoordinacion" => $this->faker->unique()->name,
+            "nomCoordinacion" => $this->faker->unique()->randomElement(['Coordinacion del CGMLTI','Coordinacion Academica','Coordinacion del Centro de Salud','Coordinacion de Finanzas','Coordinacion del centro de formacion en diseño','Coordinacion del centro para la Biodiversidad','Coordinacion del centro de formacion en moda','Coordinacion del centro de formacion en deportes','Coordinacion del centro de formacion en agricultura','Coordinacion del centro de formacion en aviacion','Coordinacion del centro de formacion en petroquimica']),
             /* "idPrograma" => Programa::all()->random()->id */
         ];
     }
