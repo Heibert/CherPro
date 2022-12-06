51a17b7ce50c25cec89ecad46de1b57914688de1
Your Name <you@example.com>
2022-12-05 22:28:45 -0500

    edit user

--- Api/resources/views/user/index.blade.php
+++ Api/resources/views/user/index.blade.php
@@ -41,6 +41,7 @@
                 <th>Telefono</th>
                 <th>Correo</th>
                 <th>Rol</th>
+                <th>Editar</th>
                 <th>Eliminar</th>
             </tr>
         </thead>
@@ -53,7 +54,12 @@
                 <td>{{ $u->apellido}}</td>
                 <td>{{ $u->telefono}}</td >
                 <td>{{ $u->email}}</td>
-                <td>{{ $u->rol}}</td>
+                <td>{{ $u->rol}}</td>   
+                <td>
+                    <a href="{{url('/user/'.$u->id.'/edit')}}">
+                        <button class="btn btn-outline-warning">Editar</button>
+                    </a> 
+                </td>
                 <td>
                     <form action="{{url('/user/'.$u->id)}}" method="post">
                         @csrf
